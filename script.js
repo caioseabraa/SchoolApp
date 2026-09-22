1cc385aad76a43560d9d9bc94896e0ccc87baa24
caioseabraa <caioslopes7@gmail.com>
2026-09-22 00:26:28 -0400

    desenvolvimento do js
    fix: problemas das marcações e caso o usuario deixe o input em branco resolvido e desenvolvimento na function createForm que cria o formuario dinamico baseado na function configForm

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function config() {
+function configForm() {
     let noteConfig = document.getElementsByName("radnote")
     let contNote = 0
     let stuconfig = document.querySelector("input#qtdenum")
     let check
     let contStu = Number(stuconfig.value)
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < noteConfig.length; i++) {
         if (noteConfig[i].checked) {
             check = i
         }
@@ -32,16 +32,62 @@ function config() {
            break
     
         default:
-            contNote = "Erro nos cases"
+            alert("Selecione uma das Opções de notas!")
             break;
     }  
-    register(contStu,contNote) 
-    alert(contNote)
+    if (!contStu || contStu <= 0) {
+        alert("Digite uma quantidade válida de alunos")
+        return
+    }
+    createForm(contStu,contNote) 
+    
 
 
 
 }
 
-function register(qtdeStu,qtdeNote) {
-    
+function createForm(qtdeStu,qtdeNote) {
+    const div = document.getElementById("stu")
+    div.innerHTML = ''
+   
+        for (let i = 1; i <= qtdeStu; i++) {
+            let bloco = document.createElement("fieldset")
+            let leg = document.createElement("legend")
+            leg.textContent = `${i}º Aluno`
+            bloco.appendChild(leg)
+
+            let itemStu = document.createElement("input")
+            itemStu.type = 'text'
+            itemStu.placeholder = "Nome do aluno: "
+            itemStu.classList.add("input-aluno")
+            bloco.appendChild(itemStu)
+            bloco.appendChild(document.createElement("br"))
+            for (let j = 1; j <= qtdeNote; j++) {
+                let itemN = document.createElement("input")
+                itemN.type = 'number'
+                itemN.placeholder = `Digite a ${j}º nota do aluno`
+                itemN.classList.add(`nota-aluno-${i}`)
+                bloco.appendChild(itemN)
+                
+            }
+
+            div.appendChild(bloco)
+            
+        }
+        
+        let btncalc = document.createElement("input")
+        btncalc.type = 'buttton'
+        btncalc.value = 'Mostrar Resultados'
+        btncalc.onclick = () => {
+            let stu = calcform(qtdeStu)
+            resultform(stu)
+
+        }
+        div.appendChild(btncalc)
+}
+function calcform(){
+
+}
+function resultform(){
+
 }
